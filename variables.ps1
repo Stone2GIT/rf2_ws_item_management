a85c174be591b6d3d46963a2c11c459f1b814ca8
tutorial <infolsimracingjustfair.org>
2024-07-31 15:37:54 +0200

    changing variable values

--- variables.ps1
+++ variables.ps1
@@ -5,10 +5,12 @@
 #
 
 # where is rfactor 2 located
-$RF2ROOT="c:\program files (x86)\steam\steamapps\common\rfactor 2"
+#$RF2ROOT="c:\program files (x86)\steam\steamapps\common\rfactor 2"
+$RF2ROOT="$HOME\rf2ds"
 
 # where is steam installed if ...
-$STEAMBASEPATH="c:\program files (x86)\steam"
+# $STEAMBASEPATH="c:\program files (x86)\steam"
+$STEAMBASEPATH="$RF2ROOT"
 
 # where do we find the downloaded workshop packages
 $RF2WORKSHOPPKGS="$STEAMBASEPATH\steamapps\workshop\content\365960"
